edf19717ec67cf4f20c83578768ce3b965128c0b
Alex Pascal <alpascal@expediagroup.com>
2024-01-09 16:50:02 +0000

    Init project

---
+++ src/test/kotlin/com/alpascal/api/shoppingapi/ShoppingApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alpascal.api.shoppingapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppingApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
