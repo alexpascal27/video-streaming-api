a73f518d8d5413119f0b2f09d3c9f17bd41dbad9
Alex Pascal <alpascal@expediagroup.com>
2024-01-12 11:38:28 +0000

    Add query for productById and get a basic call working

---
+++ src/main/kotlin/com/alpascal/api/shoppingapi/controllers/ProductController.kt
@@ -0,0 +1,15 @@
+package com.alpascal.api.shoppingapi.controllers
+
+import com.alpascal.api.shoppingapi.records.Product
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+
+@Controller
+class ProductController {
+    @QueryMapping
+    fun productById(@Argument id: String): Product {
+        return Product.getById(id)
+    }
+
+}
