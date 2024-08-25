36e270bb2eeeb6c3ca4faa4e59c035f06eee8274
alex <az506@bath.ac.uk>
2024-08-25 12:49:19 +0100

    refactor: cleanse and start from scratch

--- src/main/kotlin/com/alpascal/api/videostreamingapi/Application.kt
+++ src/main/kotlin/com/alpascal/api/videostreamingapi/Application.kt
@@ -1,4 +1,4 @@
-package com.alpascal.api.shoppingapi
+package com.alpascal.api.videostreamingapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
